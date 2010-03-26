b37d33f13acc5854adcab23e330de6d5c7254189
Christopher League <league@contrapunctus.net>
2010-03-26 08:35:16 -0400

    filter function and sets of IDs

--- gauzlib/util.py
+++ gauzlib/util.py
@@ -15,3 +15,11 @@ class GauzUtils:
                     buf.append(v)
             return ''.join(buf)
         return value
+
+    def filter(self, assets, year=None, month=None, tag=None, ids=None):
+        for a in assets:
+            if ((not year or a.date and a.date.year == year) and
+                (not month or a.date and a.date.month == month) and
+                (not tag or tag in a.tags) and
+                (not ids or a.source in ids)):
+                yield a
